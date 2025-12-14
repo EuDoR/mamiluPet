83d5ce0501d65b75a55329c93c82ce9c5fa460bc
eud0r <walljur.17@gmail.com>
2025-12-13 22:10:40 -0500

    webappcorrecto

--- main.tf
+++ main.tf
@@ -1,26 +1,26 @@
 provider "azurerm" {
-    features {}  
+  features {}
 }
 
 provider "azuread" {
-  
+
 }
 
 resource "azurerm_resource_group" "RGmamiluPet" {
-  name = "RGmamiluPet"
+  name     = "RGmamiluPet"
   location = "East US"
 }
 
 resource "azurerm_container_registry" "ACRmamiluPet" {
-  name = "ACRmamiluPet"
+  name                = "ACRmamiluPet"
   resource_group_name = azurerm_resource_group.RGmamiluPet.name
-  location = azurerm_resource_group.RGmamiluPet.location
-  sku = "Standard"
+  location            = azurerm_resource_group.RGmamiluPet.location
+  sku                 = "Standard"
 }
 
 resource "azurerm_app_service_plan" "PlanmamiluPet" {
-  name = "servicePlanMamiluPet"
-  location = azurerm_resource_group.RGmamiluPet.location
+  name                = "servicePlanMamiluPet"
+  location            = azurerm_resource_group.RGmamiluPet.location
   resource_group_name = azurerm_resource_group.RGmamiluPet.name
   sku {
     tier = "Standard"
@@ -28,14 +28,15 @@ resource "azurerm_app_service_plan" "PlanmamiluPet" {
   }
 }
 
-resource "azurerm_app_service" "WebApp1" {
-  name = "WebbApp1"
-  location = azurerm_resource_group.RGmamiluPet.location
+resource "azurerm_linux_web_app" "WebApp1" {
+  name                = "WebbApp1"
+  location            = azurerm_resource_group.RGmamiluPet.location
   resource_group_name = azurerm_resource_group.RGmamiluPet.name
-  app_service_plan_id = azurerm_app_service_plan.PlanmamiluPet.id
+  service_plan_id     = azurerm_app_service_plan.PlanmamiluPet.id
+  site_config {
 
-  app_settings =  {  
-    "mi_variable" = "valor1"
   }
-
+  app_settings = {
+    "variable" = "valor1"
+ }
 }
